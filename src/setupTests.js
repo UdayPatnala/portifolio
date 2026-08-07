c154cab3cf50b5b5864d2f66d572319aff844771
Patnala Uday Kumar <udaypatnalagithub@gmail.com>
2026-08-07 18:11:42 +0530

    Fix lint error in NextPageButton and setup IntersectionObserver mock for Vitest suite

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+  disconnect() {}
+  observe() {}
+  takeRecords() { return []; }
+  unobserve() {}
+};
